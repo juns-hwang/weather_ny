7f922931fafd10d371bb2467216b9cd3a3ec4efb
junh0921 <jun.hwang@berkeley.edu>
2025-05-17 23:21:11 -0400

    initial setup completed

---
+++ scripts/config.py
@@ -0,0 +1,15 @@
+API_KEY = "85832c8e58975a01378eaf0350698200"
+
+CITY = "New York"
+COUNTRY = "US"
+
+# Coordinates for NYC
+LAT = 40.7128
+LON = -74.0060
+
+# Path to the SQLite database
+DB_PATH = "../data/db/weather.db"
+
+# API endpoints
+CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+FORECAST_URL = "https:s//api.openweathermap.org/data/2.5/forecast"
---
+++ scripts/setup_db.py
@@ -0,0 +1,49 @@
+import sqlite3
+from config import DB_PATH
+
+def create_tables():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS current_weather (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        city TEXT,
+        country TEXT,
+        datetime_utc TIMESTAMP,
+        temp_k REAL,
+        feels_like_k REAL,
+        humidity INTEGER,
+        pressure INTEGER,
+        wind_speed REAL,
+        wind_deg INTEGER,
+        weather_description TEXT,
+        UNIQUE(city, datetime_utc)
+    );
+    ''')
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS forecast_weather (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        city TEXT,
+        country TEXT,
+        forecast_time_utc TIMESTAMP,
+        forecast_created_at TIMESTAMP,
+        temp_k REAL,
+        feels_like_k REAL,
+        humidity INTEGER,
+        pressure INTEGER,
+        wind_speed REAL,
+        wind_deg INTEGER,
+        weather_description TEXT,
+        UNIQUE(city, forecast_time_utc)
+    );
+    ''')
+
+    conn.commit()
+    conn.close()
+    
+    print("Table created.")
+    
+if __name__ == "__main__":
+    create_tables()
